8ddafdd87fb7e7d44df425a2c56e4497f6bbc2a3
Nalla Sai Krishna Reddy <105940056+sakrin18072@users.noreply.github.com>
2023-07-23 16:41:43 +0530

    Port number configured

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import PreemptivePriorityRoutes from './routes/PreemptivePriorityRoutes.js'
 import PreemptiveSJFRoutes from './routes/PreemptiveSJFRoutes.js'
-
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.listen(8080,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("Server is live on port 8080");
 })
 app.use('/priority',PreemptivePriorityRoutes);
