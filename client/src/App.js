850375c681b1740490abfd3c0a1296ee4e15a650
Nalla Sai Krishna Reddy <105940056+sakrin18072@users.noreply.github.com>
2023-07-23 16:54:47 +0530

    app.js edited

--- client/src/App.js
+++ client/src/App.js
@@ -13,14 +13,14 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/OS-cbp-vnr" element={<Home />} />
-        <Route path="/OS-cbp-vnr/sjf/non-preemptive" element={<SJFNonPreemptive/>} />
-        <Route path="/OS-cbp-vnr/sjf/preemptive" element={<SJFPreemptive/>} />
-        <Route path="/OS-cbp-vnr/sjf" element={<SJF/>} />
-        <Route path="/OS-cbp-vnr/fcfs" element={<FCFS/>} />
-        <Route path="/OS-cbp-vnr/priority/non-preemptive" element={<PriorityNonPreemptive/>} />
-        <Route path="/OS-cbp-vnr/priority/preemptive" element={<PriorityPreemptive/>} />
-        <Route path="/OS-cbp-vnr/priority" element={<Priority/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/sjf/non-preemptive" element={<SJFNonPreemptive/>} />
+        <Route path="/sjf/preemptive" element={<SJFPreemptive/>} />
+        <Route path="/sjf" element={<SJF/>} />
+        <Route path="/fcfs" element={<FCFS/>} />
+        <Route path="/priority/non-preemptive" element={<PriorityNonPreemptive/>} />
+        <Route path="/priority/preemptive" element={<PriorityPreemptive/>} />
+        <Route path="/priority" element={<Priority/>} />
       </Routes>
     </div>
   );
